088ef9a95c5b1175711796b0e1abea47336e070f
Thiago Bignotto <bignotto@gmail.com>
2020-06-05 00:17:46 -0300

    aula2 30 minutos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/users", (req, res) => {
   console.log("listagem de usuários");
   res.send("hello big! denovo");
 });
+
+app.get("/users/:id", (req, res) => {});
 
 app.listen(3333);
