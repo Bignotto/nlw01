a7fd58b7eaf68b5806831c53b0920df5139ca563
Thiago Bignotto <bignotto@gmail.com>
2020-06-05 23:02:21 -0300

    aula2 cadastro de novos pontos de coleta

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -7,7 +7,7 @@ export async function up(knex: Knex) {
     table.string("name").notNullable();
     table.string("email").notNullable();
     table.string("telefone").notNullable();
-    table.decimal("latitute").notNullable();
+    table.decimal("latitude").notNullable();
     table.decimal("longitude").notNullable();
     table.string("city").notNullable();
     table.string("uf", 2).notNullable();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
@@ -10,6 +11,56 @@ routes.get("/users/:id", (req, res) => {});
 
 routes.post("/users", (req, res) => {
   //return res.json(res.body)
+});
+
+routes.get("/items", async (req, res) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return res.json(serializedItems);
+});
+
+routes.post("/points", async (req, res) => {
+  const {
+    name,
+    email,
+    telefone,
+    latitude,
+    longitude,
+    city,
+    uf,
+    country,
+    items,
+  } = req.body;
+
+  const ids = await knex("points").insert({
+    image: "fakeimg",
+    name,
+    email,
+    telefone,
+    latitude,
+    longitude,
+    city,
+    uf,
+    country,
+  });
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex("point_items").insert(pointItems);
+
+  return res.json({ response: "success!" });
 });
 
 export default routes;
