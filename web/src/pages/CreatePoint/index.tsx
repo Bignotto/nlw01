4eadedaa0b40d794b3be3354776f83a66154923e
Thiago Bignotto <bignotto@gmail.com>
2020-06-06 18:18:52 -0300

    aula3 formulario criado

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,9 +3,104 @@ CREATEPOINT
 */
 
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import "./styles.css";
+import logo from "../../assets/logo.svg";
 
 const CreatePoint = () => {
-  return <h1>Big Rules on CreatePoint page</h1>;
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="logo" />
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Home
+        </Link>
+      </header>
+      <form>
+        <h1>
+          Cadastro de <br />
+          Ponto de Coleta
+        </h1>
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+          <div className="field">
+            <label htmlFor="name">Nome da Entidade:</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="telefone">Telefone:</label>
+              <input type="text" name="telefone" id="telefone" />
+            </div>
+            <div className="field">
+              <label htmlFor="email">E-mail:</label>
+              <input type="email" name="email" id="email" />
+            </div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Apnte o endereço no mapa:</span>
+          </legend>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">
+                <select name="uf" id="uf">
+                  <option value="0">Selecione o Estado</option>
+                </select>
+              </label>
+            </div>
+            <div className="field">
+              <label htmlFor="city">
+                <select name="city" id="city">
+                  <option value="0">Selecione a Cidade</option>
+                </select>
+              </label>
+            </div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Items</h2>
+            <span>Selecione um ou mais itens abaixo:</span>
+          </legend>
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="temp" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="temp" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="temp" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="temp" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="temp" />
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="temp" />
+              <span>Óleo de Cozinha</span>
+            </li>
+          </ul>
+        </fieldset>
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePoint;
